8f84c17b59442c22ca04932fbe0cd399ef53613c
hp-1e <1754049249@qq.com>
2025-01-23 20:10:51 +0800

    Add files via upload

--- kubejs/startup_scripts/hpdlc/dim_hpdlc/dim_hpdlc_item_register.js
+++ kubejs/startup_scripts/hpdlc/dim_hpdlc/dim_hpdlc_item_register.js
@@ -16,11 +16,12 @@ StartupEvents.registry('item', event => {
         return builder
       }
     
-    //恒温器
+    //温度调节器
      registerOrgan(new Organ('hpdlc:thermostat')
         .addScore('health', 1)
         .addScore('burning_point', 40)
         .addScore('freezing_point', -40)
+        .addTextLines('alt',[LEADING_SYMBOL,Text.gray({"translate":"hpdlc.tooltips.jixiefeisheng.2"})])
         .addTextLines('default', [Text.gray({ "translate": "hpdlc.tooltips.thermostat.1" })])
         .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat.2" })])
         .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat.3" })])
@@ -29,5 +30,37 @@ StartupEvents.registry('item', event => {
         .tag('kubejs:player_tick_only')
         .tag('kubejs:machine')
         .tag('kubejs:resource');
+    
+    //温度调节器(高温)
+     registerOrgan(new Organ('hpdlc:thermostat_hot')
+        .addScore('health', 1)
+        .addScore('burning_point', 40)
+        .addScore('freezing_point', -40)
+        .addTextLines('alt',[LEADING_SYMBOL,Text.gray({"translate":"hpdlc.tooltips.jixiefeisheng"})])
+        .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat_hot.1" })])
+        .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat_hot.2" })])
+        .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat_hot.3" })])
+        .build())
+        .texture('hpdlc:item/dim_hpdlc/thermostat_hot')
+        .tag('kubejs:rclick_only')
+        .tag('kubejs:player_tick_only')
+        .tag('kubejs:machine')
+        .tag('kubejs:resource');
+
+    //温度调节器(低温)
+     registerOrgan(new Organ('hpdlc:thermostat_cold')
+        .addScore('health', 1)
+        .addScore('burning_point', 40)
+        .addScore('freezing_point', -40)
+        .addTextLines('alt',[LEADING_SYMBOL,Text.gray({"translate":"hpdlc.tooltips.jixiefeisheng"})])
+        .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat_cold.1" })])
+        .addTextLines('alt', [LEADING_SYMBOL, Text.gray({ "translate": "hpdlc.tooltips.thermostat_cold.2" })])
+        .build())
+        .texture('hpdlc:item/dim_hpdlc/thermostat_cold')
+        .tag('kubejs:rclick_only')
+        .tag('kubejs:player_tick_only')
+        .tag('kubejs:machine')
+        .tag('kubejs:resource');
+
 
     })
